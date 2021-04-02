b9266fcaea39128fbe9f72f287ca2cce245adb4c
Sacha Wharton <sachajw@gmail.com>
2021-04-02 05:06:48 +0200

    nginx config testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,6 @@ set -e
 envsubst < /cert.pem > /etc/ssl/cert.pem
 envsubst < /privkey.pem > /etc/ssl/privkey.pem
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/nginx.conf
+
 nginx -g 'daemon off;'
